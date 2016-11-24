eccf961c218a98aa1d085ea639052bca4c37285a
Thomas Cools <tacools10@gmail.com>
2016-11-24 16:18:09 +0100

    Working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,29 +1,29 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail, only[:new, :create, :destroy]
+  before_action :find_cocktail, only: [:new, :create, :destroy]
 
   def new
     @dose = Dose.new
   end
 
   def create
-    @dose = @cocktail.dose.build(dose_params)
+    @dose = @cocktail.doses.build(dose_params)
     @dose.save
     redirect_to cocktail_path(@cocktail)
   end
 
   def destroy
-    @cocktail.dose.destroy
+    @cocktail.doses.last.destroy
     redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 
   def find_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
-
 
 end
